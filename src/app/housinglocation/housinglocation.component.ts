8960a891396c51ed37169f40925fae75f091630b
DESKTOP-UFQIETO <probonly12345@gmail.com>
2023-08-24 17:20:40 +0530

    commit

---
+++ src/app/housinglocation/housinglocation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housinglocation',
+  templateUrl: './housinglocation.component.html',
+  styleUrls: ['./housinglocation.component.css']
+})
+export class HousinglocationComponent {
+
+}
