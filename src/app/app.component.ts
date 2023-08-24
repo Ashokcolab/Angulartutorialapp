96df382b171d2d8fa9c32c524757b09c255d72dd
DESKTOP-UFQIETO <probonly12345@gmail.com>
2023-08-24 18:35:53 +0530

    till adding houselocation component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `<main>
   <header class="brand-name">
-    <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    <img class="brand-logo" src="https://static.vecteezy.com/system/resources/thumbnails/008/075/444/small/the-logo-of-home-housing-residents-real-estate-with-a-concept-that-presents-rural-nature-with-a-touch-of-leaves-and-sunflowers-vector.jpg" alt="logo" aria-hidden="true">
   </header>
   <section class="content">
     <app-home></app-home>
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { HousinglocationComponent } from '../housinglocation/housinglocation.component';
 
 @Component({
   selector: 'app-home',
   template:`
-  <section>
+   <section>
     <form>
       <input type="text" placeholder="Filter by city">
       <button class="primary" type="button">Search</button>
     </form>
+  </section>
+  <section class="results">
+    <app-housinglocation></app-housinglocation>
   </section>
   `,
   styleUrls: ['./home.component.css']
