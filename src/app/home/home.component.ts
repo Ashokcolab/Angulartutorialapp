e49aaba402538b18ed6e723367374acdec04b23b
DESKTOP-UFQIETO <probonly12345@gmail.com>
2023-08-25 15:42:01 +0530

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { HousingLocation } from './../housinglocation';
+// import { Component } from '@angular/core';
 import { HousinglocationComponent } from '../housinglocation/housinglocation.component';
+import {Component,inject} from '@angular/core';
+import { core } from '@angular/compiler';
+import { HousingService } from '../housing.service';
+
 
 @Component({
   selector: 'app-home',
@@ -11,11 +16,20 @@ import { HousinglocationComponent } from '../housinglocation/housinglocation.com
     </form>
   </section>
   <section class="results">
-    <app-housinglocation></app-housinglocation>
+    <app-housinglocation
+    *ngFor="let housingLocation of housingLocationList"
+    [housingLocation]="housingLocation"
+    ></app-housinglocation>
   </section>
   `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  readonly baseUrl = 'assets/images';
+  housingLocationList: HousingLocation[] = ([])
+  housingService:HousingService = inject(HousingService);
+  constructor(){
+    this.housingLocationList=this.housingService.getAllHousingLocations();
+  }
 }
+
--- src/app/housinglocation/housinglocation.component.ts
+++ src/app/housinglocation/housinglocation.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HousingLocation } from './../housinglocation';
+import { Component,Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-housinglocation',
-  templateUrl: './housinglocation.component.html',
+  template:`
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+  </section>
+  `,
   styleUrls: ['./housinglocation.component.css']
 })
 export class HousinglocationComponent {
+  @Input() housingLocation!:HousingLocation;
 
 }
